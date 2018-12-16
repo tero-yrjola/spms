d1120da4f2bd5148a5a020f9d4d2faedae4402fa
tero-yrjola <yrjola.tero@gmail.com>
2018-12-16 10:54:57 +0200

    Remove unnecessary stuff

---
+++ src/Base.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Base extends Component {
+    render() {
+        return (
+            <div className="Base">
+
+            </div>
+        );
+    }
+}
+
+export default Base;
